3dc05764834459629e321cb75415d4a03ca1f2fc
Agnese Bockane <agnese.bockane@gmail.com>
2019-02-27 09:31:45 +0100

    not done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "tasks/:id", to: "tasks#show"
+  get "tasks", to: "tasks#index"
+
+
+  # get 'tasks/edit'
+  # get 'tasks/update'
+  # get 'tasks/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
